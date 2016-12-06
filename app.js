a78e15ae9fed5eeb6f5c2bc23dbfbc96e1f9adb0
Jeny Amatya <jeny_amatya@dialog.com.au>
2016-12-06 14:40:00 +0930

    Another version

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-(function() { //iife: imeediately-invoked function expression
+(function() { //iife: immediately-invoked function expression
   'use strict';
-  angular.module('myFirstApp', [])
+
+  angular.module('NameCalculator', [])
+    .controller('NameCalculatorCtrl', function($scope){
+        $scope.name="";
+        $scope.totalValue = 0;
+
+        $scope.displayNumeric = function(){
+            var totalNameValue = calculateNumericForString($scope.name);
+            $scope.totalValue = totalNameValue;
+        };
+    });
+
+    function calculateNumericForString(string){
+      var totalStringValue = 0;
+      for(var i=0; i<string.length;i++){
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+
+/*  angular.module('myFirstApp', [])
   .controller('MyFirstController', function($scope){ //share data between view and view model
       $scope.name = "Jen";
       $scope.sayHello = function(){
         return "Hello Coursera";
       };
-  });
+  });*/
 })();
