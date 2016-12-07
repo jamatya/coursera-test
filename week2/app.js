ebe5b1cd322347edf85c8a55f040940dea86cd1f
Jeny Amatya <jeny_amatya@dialog.com.au>
2016-12-07 16:15:08 +0930

    Custom Filter 2

--- week2/app.js
+++ week2/app.js
@@ -2,16 +2,17 @@
   'use strict';
 
     angular.module('MsgApp', [])
-    .controller('MsgCtrl', MsgCtrl);
+    .controller('MsgCtrl', MsgCtrl)
+    .filter('loves', LovesFilter);
 
-    MsgCtrl.$inject = ['$scope', '$filter'];
-    function MsgCtrl($scope, $filter){
+    MsgCtrl.$inject = ['$scope', '$filter', 'lovesFilter'];
+    function MsgCtrl($scope, $filter, lovesFilter){
       $scope.name = "Jen";
       $scope.stateOfBeing = 'hungry';
       $scope.feedCost = 0.57;
 
       $scope.sayMessage = function(){
-        var msg = "Eat healthy snacks!";
+        var msg = "likes eating healthy snacks!";
         var output = $filter('uppercase')(msg);
         return output;
       };
@@ -19,5 +20,19 @@
       $scope.feedMe = function(){
         $scope.stateOfBeing = 'fed';
       };
-    };
+
+      $scope.sayLovesMessage = function(){
+        var msg = "likes eating healthy snacks!";
+        msg = lovesFilter(msg);
+        return msg;
+      };
+    }
+
+    function LovesFilter(){
+      return function(input){
+        input = input || "";
+        input = input.replace("likes", "loves");
+        return input;
+      };
+    }
 })();
